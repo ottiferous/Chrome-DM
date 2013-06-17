f5be60a33ed6cf062e4d0078d1164a833a288828
Andrew <github@andrewmarrone.com>
2013-06-17 15:39:29 -0700

    Working
    displays info in JSON dump, but it works

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import sys
 import webapp2
 
 from apiclient.discovery import build
+from google.appengine.ext import webapp
 from oauth2client.file import Storage
 from oauth2client.appengine import OAuth2Decorator
+from oauth2client.appengine import OAuth2DecoratorFromClientSecrets
 from oauth2client.client import AccessTokenRefreshError
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.tools import run
@@ -16,8 +18,8 @@ from oauth2client.tools import run
 
 # OAuth Token
 decorator = OAuth2Decorator(
-   client_id='1002667537078-s6rptef9h1s7jb72ssnbvr376gs0vgds.apps.googleusercontent.com',
-   client_secret='BaHkkaBxYk9d_b6cHE2iYGQ6',
+   client_id='1002667537078-pscobeqht92tkpnjg8cghf1ssaafkrvd.apps.googleusercontent.com',
+   client_secret='bgi3D7iua008KJ4SBr0F45nZ',
    scope='https://www.googleapis.com/auth/admin.directory.device.chromeos')
 
 service = build('admin', 'directory_v1')
@@ -26,12 +28,12 @@ service = build('admin', 'directory_v1')
 class Main(webapp2.RequestHandler):
    @decorator.oauth_required
    def get(self):  
-      print 'Starting'
+      http = decorator.http()
       if decorator.has_credentials():
-         request = service.chromeosdevices().list(customerId='my_customer').execute()
-         self.response.write(json.dumps(request))
+         request = service.chromeosdevices().list(customerId='my_customer').execute(decorator.http())
+         self.response.write(request)
       else:
-         self.response.write(json.dumps({'error' : 'No credentials'}))
+         self.response.write('Y\'all gonna need some credentials')
 
 
 app = webapp2.WSGIApplication( [ 
