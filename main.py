0e89e2df931ff2b4e29faacdf0838f7315d50025
Andrew <github@andrewmarrone.com>
2013-07-18 17:10:09 -0700

    Use methods throughout main
    Uses the methods from hortator to setup the OAuth handshake, etc

--- main.py
+++ main.py
@@ -24,11 +24,13 @@ from secretlist import OauthSecrets
 from hortator import GetChromeManifest
 from hortator import BuildChromeManifest
 
+#
 # OAuth Token using list unpacking from secret files
-decorator = OAuth2Decorator( *(OauthSecrets('local')) )
+#
+global RUNLOCATION
+RUNLOCATION = 'local'
+decorator = OAuth2Decorator( *(OauthSecrets(RUNLOCATION)) )
 
-# comment out the line below to work offline
-service = build('admin', 'directory_v1')
 
 # Jinja Stuff Goes Here
 jinja_environment = jinja2.Environment(autoescape=True,
@@ -38,60 +40,46 @@ jinja_environment = jinja2.Environment(autoescape=True,
 class Main(webapp2.RequestHandler):
    @decorator.oauth_required
    def get(self):
-      self.response.headers['Content-Type'] = 'text/csv'  
-      http = decorator.http()
-      if decorator.has_credentials():
-         request = service.chromeosdevices().list(customerId='my_customer').execute(decorator.http())
-         devices = request['chromeosdevices']
-         while 'nextPageToken' in request:
-            request = service.chromeosdevices().list(customerId='my_customer', pageToken=request['nextPageToken']).execute()
-            devices.append(result['chromeosdevices'])
-         manifest = {
-            'annotatedLocation': u'','annotatedUser': u'','bootMode': u'','deviceId': u'',
-            'firmwareVersion': u'','kind': u'','lastEnrollmentTime': u'','lastSync': u'',
-            'macAddress': u'','meid': u'','model': u'','notes': u'','orderNumber': u'',
-            'orgUnitPath': u'','osVersion': u'','platformVersion': u'','serialNumber': u'',
-            'status': u'','supportEndDate': u'','willAutoRenew': u'' 
-         }         
-         # create a manfiest with blanks for empty fields
-         deviceList = []
-         for _ in devices:
-            manifest.update(_)
-            deviceList.append(manifest.values())
-         
-         # Write the Header info
-         for _ in manifest.keys():
-            self.response.write( _ + "\t")
+      response = GetChromeManifest(RUNLOCATION, decorator)
+      manifest = {
+         'annotatedLocation': u'','annotatedUser': u'','bootMode': u'','deviceId': u'',
+         'firmwareVersion': u'','kind': u'','lastEnrollmentTime': u'','lastSync': u'',
+         'macAddress': u'','meid': u'','model': u'','notes': u'','orderNumber': u'',
+         'orgUnitPath': u'','osVersion': u'','platformVersion': u'','serialNumber': u'',
+         'status': u'','supportEndDate': u'','willAutoRenew': u'' 
+      }         
+      response = BuildChromeManifest(manifest, response)         
+      
+      # Write the Header info
+      for _ in manifest.keys():
+         self.response.write( _ + "\t")
+      self.response.write("\n")
+      
+      
+      # Begin writing the device info lines
+      for row in response:
+         for _ in row:
+            try:
+               self.response.write(str(_) + "\t")
+            except:
+               print "ERROR parsing: ", _
          self.response.write("\n")
-         
-         
-         # Begin writing the device info lines
-         for row in deviceList:
-            for _ in row:
-               try:
-                  self.response.write(str(_) + "\t")
-               except:
-                  print "ERROR parsing: ", _
-            self.response.write("\n")
-            
-      else:
-         self.response.write('Y\'all gonna need some credentials')
 
 class MakeCSV(webapp2.RequestHandler):
-   
+   @decorator.oauth_required
    def get(self):
-      response = GetChromeManifest('local')
+      response = GetChromeManifest(RUNLOCATION, decorator)
       
       manifestTemplate = {
-         'annotatedUser': u'', 'kind': u'','lastEnrollmentTime': u'','lastSync': u'',
-         'model': u'','notes': u'','orgUnitPath': u'','osVersion': u'',
-         'platformVersion': u'','serialNumber': u'' }
+         'annotatedUser': u'', 'lastEnrollmentTime': u'','lastSync': u'',
+         'notes': u'','orgUnitPath': u'','osVersion': u'',
+         'platformVersion': u'','serialNumber': u''
+      }
          
       response = BuildChromeManifest(manifestTemplate, response)
-      
-      readableList = ['User', 'Kind', 'Enrollment Time', 'Last Sync', 
-                'Model', 'Notes', 'OU Path', 'OS Version', 
-                'Platform Version', 'Serial Number']
+      print response[1]
+      readableList = ['User', 'First Enrollment', 'Serial Number', 'Last Sync', 
+                'Platform Version', 'Notes', 'OS Version', 'OU Path']
                
       template = jinja_environment.get_template('index.html')
       self.response.out.write(template.render(header_list=readableList, device_page=response))
@@ -105,12 +93,11 @@ class RenderMain(webapp2.RequestHandler):
          'platformVersion': u'','serialNumber': u'' }
       built_list = []
       devices = False
-
       http = decorator.http()
       if decorator.has_credentials():
          request = service.chromeosdevices().list(customerId='my_customer').execute(decorator.http())
          devices = request['chromeosdevices']
-      
+ 
          while 'nextPageToken' in request:
             request = service.chromeosdevices().list(customerId='my_customer', pageToken=request['nextPageToken']).execute()
             devices.append(result['chromeosdevices'])
@@ -125,8 +112,7 @@ class RenderMain(webapp2.RequestHandler):
          print "[DEVICES]: ", devices
          print "[BUILT_LIST]: ", built_list
          print "[DECORATOR]: ", decorator.iteritems()
-      
-      
+
       readable_list = ['User', 'Kind', 'Enrollment Time', 'Last Sync', 
                 'Model', 'Notes', 'OU Path', 'OS Version', 
                 'Platform Version', 'Serial Number']
