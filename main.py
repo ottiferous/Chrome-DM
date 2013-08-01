638ad00fad33a0b815b00e780a93cbdca0ecf8fb
Andrew <github@andrewmarrone.com>
2013-07-31 18:59:18 -0700

    Created error handling and local runtime check
    new MainHandler class is designed to deal with all error handling

--- main.py
+++ main.py
@@ -23,15 +23,16 @@ from oauth2client.tools import run
 from secretlist import OauthSecrets
 from hortator import GetChromeManifest
 from hortator import BuildChromeManifest
-from hortator import FakeChromeManifest
 from hortator import StatsFromManifest
 
 # Determine run location
 global RUNLOCATION
-if os.environ['SERVER_SOFTWARE'].startswith('Development'):
-   RUNLOCATION = 'local'
+if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
+   RUNLOCATION = 'debug'
+   debug = True
 else:
    RUNLOCATION = 'online'
+   debug = False
 
 #
 # OAuth Token using list unpacking from secret files
@@ -43,26 +44,43 @@ decorator = OAuth2Decorator( *(OauthSecrets(RUNLOCATION)) )
 jinja_environment = jinja2.Environment(autoescape=True,
    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'pagetemplates')))
 
+class BaseHandler(webapp2.RequestHandler):
+  'All your Error Handling are belong to us'
+  def handle_exception(self, exception, debug):
+    # Log the error
+    logging.exception(exception)
+    print "==================================="
+    print exception._get_reason()
+    print type(exception)
+    print "==================================="
 
-class Main(webapp2.RequestHandler):
+    if isinstance(exception, webapp2.HTTPException):
+      self.response.set_status(exception.code)
+    else:
+      self.response.set_status(500)
+
+    template = jinja_environment.get_template('error.html')
+    self.response.out.write(template.render(messageobj=exception._get_reason()))
+
+class Main(BaseHandler):
   def get(self):
     template = jinja_environment.get_template('index.html')
     self.response.out.write(template.render())
   
 
-class SetupPage(webapp2.RequestHandler):
+class SetupPage(BaseHandler):
   def get(self):
     template = jinja_environment.get_template('setup.html')
     self.response.out.write(template.render())
   
 
-class AboutPage(webapp2.RequestHandler):
+class AboutPage(BaseHandler):
   def get(self):
       template = jinja_environment.get_template('about.html')
       self.response.out.write(template.render())
   
 
-class StatsPage(webapp2.RequestHandler):
+class StatsPage(BaseHandler):
    @decorator.oauth_required
    def get(self):
       response = GetChromeManifest(decorator)
@@ -72,12 +90,12 @@ class StatsPage(webapp2.RequestHandler):
          'platformVersion': u'','serialNumber': u''
       }
       stats = StatsFromManifest(response)
-         
+       
       response = BuildChromeManifest(manifestTemplate, response)
       readableList = ['User', 'First Enrollment', 'Serial Number', 'Last Sync', 'Platform Version', 'Notes', 'OS Version', 'OU Path']
-         
+       
       template = jinja_environment.get_template('statspage.html')
-      
+    
       self.response.out.write(template.render(
          header_list=readableList, device_page=response, 
             Channel=stats['Channel'], OUPath=stats['OUPath'], 
@@ -85,8 +103,8 @@ class StatsPage(webapp2.RequestHandler):
             total=len(response)
          )
       )
-
-class MakeCSV(webapp2.RequestHandler):
+      
+class MakeCSV(BaseHandler):
    @decorator.oauth_required
    def get(self):
       response = GetChromeManifest(decorator)
@@ -115,9 +133,13 @@ class MakeCSV(webapp2.RequestHandler):
             try:
                self.response.write("\"" + str(_) + "\"" + ",")
             except:
-               print "ERROR parsing: ", _
+               print "[ERROR parsing]: ", _
          self.response.write("\n")
-         
+
+def handle_403(request, response, exception):
+   logging.exception(exception)
+   response.write('Ooops! You\'re gonna need authorization to do that')
+   response.set_status(403)         
     
 app = webapp2.WSGIApplication( [ 
    ( '/', Main),
@@ -126,4 +148,6 @@ app = webapp2.WSGIApplication( [
    ( '/about', AboutPage),
    ( '/setup', SetupPage),
    (decorator.callback_path, decorator.callback_handler())
-], debug=True )
+], debug=debug )
+
+app.error_handlers[403] = handle_403
