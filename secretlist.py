b431f14507393d32d7ede952b6844006d9d9f65c
Andrew <github@andrewmarrone.com>
2013-10-25 13:46:34 -0700

    OAuth decorator file
    missing file that includes the setup of OAuth2Decorator as it was
    requested by a lot of people

---
+++ secretlist.py
@@ -0,0 +1,19 @@
+#
+# Setup OAuth2Decorator Class
+#
+
+
+def CreateDecorator(environment):
+   'Creates the proper OAuth2Decorator for use with AppEngine'
+
+   if environment = 'local':    #for running locally through AppEngine SDK
+      return OAuth2Decorator(
+         client_id='YOUR_CLIENT_ID_HERE',
+         client_secret='YOU_CLIENT_SECRET_HERE',
+         scope='https://www.googleapis.com/auth/admin.directory.device.chromeos')
+   
+   else:
+      return OAuth2Decorator(
+         client_id='YOUR_CLIENT_ID_HERE,
+         client_secret='YOU_CLIENT_SECRET_HERE',
+         scope='https://www.googleapis.com/auth/admin.directory.device.chromeos')
