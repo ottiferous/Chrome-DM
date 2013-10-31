ca9fd597b3a490468e756c8c54bf59d7cdd61abe
Andrew <github@andrewmarrone.com>
2013-10-31 15:55:05 -0700

    final cleanup and sanitizing

--- secretlist.py
+++ secretlist.py
@@ -2,21 +2,19 @@
 # Setup OAuth2Decorator Class
 #
 
-from oauth2client.appengine import Oauth2Decorator
 from oauth2client.appengine import OAuth2Decorator
 
 def CreateDecorator(environment):
    'Creates the proper OAuth2Decorator for use with AppEngine'
 
-   if environment = 'local':    #for running locally through AppEngine SDK
    if environment != 'local':
       return OAuth2Decorator(
-         client_id='YOUR_CLIENT_ID_HERE',
-         client_secret='YOU_CLIENT_SECRET_HERE',
+         client_id='',
+         client_secret='',
          scope='https://www.googleapis.com/auth/admin.directory.device.chromeos')
    
    else:
       return OAuth2Decorator(
-         client_id='YOUR_CLIENT_ID_HERE,
-         client_secret='YOU_CLIENT_SECRET_HERE',
+         client_id='',
+         client_secret='',
          scope='https://www.googleapis.com/auth/admin.directory.device.chromeos')
